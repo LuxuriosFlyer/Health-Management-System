183fd1e999b6d34aac13cb90b9492d518317d413
LuxuriosFlyer <88792978+LuxuriosFlyer@users.noreply.github.com>
2022-07-08 20:51:55 +0530

    Delete DSA directory

--- DSA/remove_spaces.cpp
+++
@@ -1,6 +0,0 @@
-// for(int i=0;i<s.size();i++)
-//         {
-//             if(s[i]==' ')
-//             s.erase(remove(s.begin(),s.end(),' '),s.end());
-//         }
-//         return s;
--- DSA/rotate_string_lc.cpp
+++
@@ -1,7 +0,0 @@
-// bool rotateString(string s, string goal) {
-//      string temp=s+s;
-//         if(s.length()!=goal.length())
-//             return false;
-//         if(temp.find(goal)==string::npos)
-//             return false;
-//         return true;
